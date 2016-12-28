5d7e3fc6b4de8ea36692da66fb78376d1b75d90d
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-28 15:34:15 -0600

    html added

---
+++ cards.js
@@ -0,0 +1,15 @@
+//take value of input box
+
+//put in new bootstrap card
+  //new card has
+    //html for new card
+      var newCard = <div class="col-sm-6">
+    <div class="card card-block">
+      <h3 class="card-title">Special title treatment</h3>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    //text from inputbox
+    //delete button
+
+//delete button function
